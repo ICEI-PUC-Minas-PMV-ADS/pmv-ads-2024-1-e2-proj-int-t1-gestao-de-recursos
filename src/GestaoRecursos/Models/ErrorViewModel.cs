e9915baffadba0b7ab9294b4ddf0126207ac94e5
jesternook <nicocossta@outlook.com>
2024-04-16 20:28:56 -0400

    feat: Criação inicial de projeto MVC.

---
+++ src/GestaoRecursos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoRecursos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
