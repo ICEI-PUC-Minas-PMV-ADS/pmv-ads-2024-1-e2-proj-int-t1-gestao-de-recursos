173c97e484a7abf668574f8d7667a2b8ec05f484
Thiago Bastani <thiagobastani.tb@gmail.com>
2024-04-22 16:15:48 -0300

    Add TipoProduto CRUD

---
+++ src/GestaoRecursos/Controllers/TipoProdutoController.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Context;
+using Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GestaoRecursos.Controllers
+{
+    [Authorize]
+    public class TipoProdutoController : Controller
+    {
+        private readonly GestaoContext _context;
+
+        public TipoProdutoController(GestaoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TipoProduto
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TipoProdutos.ToListAsync());
+        }
+
+        // GET: TipoProduto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoProduto = await _context.TipoProdutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipoProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoProduto);
+        }
+
+        // GET: TipoProduto/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoProduto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome")] TipoProduto tipoProduto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tipoProduto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoProduto);
+        }
+
+        // GET: TipoProduto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoProduto = await _context.TipoProdutos.FindAsync(id);
+            if (tipoProduto == null)
+            {
+                return NotFound();
+            }
+            return View(tipoProduto);
+        }
+
+        // POST: TipoProduto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome")] TipoProduto tipoProduto)
+        {
+            if (id != tipoProduto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tipoProduto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipoProdutoExists(tipoProduto.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tipoProduto);
+        }
+
+        // GET: TipoProduto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tipoProduto = await _context.TipoProdutos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tipoProduto == null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoProduto);
+        }
+
+        // POST: TipoProduto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tipoProduto = await _context.TipoProdutos.FindAsync(id);
+            if (tipoProduto != null)
+            {
+                _context.TipoProdutos.Remove(tipoProduto);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipoProdutoExists(int id)
+        {
+            return _context.TipoProdutos.Any(e => e.Id == id);
+        }
+    }
+}
