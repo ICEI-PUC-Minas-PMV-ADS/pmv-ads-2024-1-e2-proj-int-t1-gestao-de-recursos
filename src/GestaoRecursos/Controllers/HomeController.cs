5660f118f9d06c17053d3cfe67500ec057ab0dd0
Thiago Bastani <thiagobastani.tb@gmail.com>
2024-04-22 15:17:12 -0300

    Add Authorization to HomeController

--- src/GestaoRecursos/Controllers/HomeController.cs
+++ src/GestaoRecursos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using GestaoRecursos.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,11 +14,13 @@ namespace GestaoRecursos.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
