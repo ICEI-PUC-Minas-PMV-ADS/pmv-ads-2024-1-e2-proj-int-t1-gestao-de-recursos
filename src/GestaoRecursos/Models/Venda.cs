45ac8bbb8cd0b71e760ed4f101c97c9a532999c1
TomazMPP <tomazmppontes@gmail.com>
2024-04-25 15:29:03 -0300

    ✨ feat: Vendas

---
+++ src/GestaoRecursos/Models/Venda.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+namespace GestaoRecursos.Models;
+
+public class Venda
+{
+    public int Id { get; set; }
+    public List<Produto> Produtos { get; set; }
+    public string NotaFiscal { get; set; }
+    public bool Ativo { get; set; } = true;
+    public DateTime? DataCriacao { get; set; }
+    public DateTime? DataAlteracao { get; set; }
+}
