8b2651f2c1eea8b62597903c677a42e98c1c080d
TomazMPP <tomazmppontes@gmail.com>
2024-04-29 14:53:54 -0300

    feat: Compra

---
+++ src/GestaoRecursos/Controllers/CompraController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Context;
+using GestaoRecursos.Models;
+
+namespace GestaoRecursos.Controllers
+{
+    public class CompraController : Controller
+    {
+        private readonly GestaoContext _context;
+
+        public CompraController(GestaoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Compra
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Compra.ToListAsync());
+        }
+
+        // GET: Compra/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compra
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // GET: Compra/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Compra/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ativo,DataCriacao,DataAlteracao,Quantidade")] Compra compra)
+        {
+            compra.DataCriacao = DateTime.Now;
+            compra.DataAlteracao = DateTime.Now;
+            _context.Add(compra);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Compra/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compra.FindAsync(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+            return View(compra);
+        }
+
+        // POST: Compra/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ativo,DataCriacao,DataAlteracao,Quantidade")] Compra compra)
+        {
+            compra.DataAlteracao = DateTime.Now;
+            if (id != compra.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Update(compra);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompraExists(compra.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+                return RedirectToAction(nameof(Index));
+
+            }
+
+        // GET: Compra/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compra
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // POST: Compra/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var compra = await _context.Compra.FindAsync(id);
+            if (compra != null)
+            {
+                _context.Compra.Remove(compra);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompraExists(int id)
+        {
+            return _context.Compra.Any(e => e.Id == id);
+        }
+    }
+}
