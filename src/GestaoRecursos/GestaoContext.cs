547e18710355294a03763cefdce826e1688e5cf2
jesternook <nicocossta@outlook.com>
2024-05-10 18:46:24 -0400

    Revert "feat: Criação de migrations, conexão com banco de dados das tabelas Usuarios e Perfis, configuração de roles"
    This reverts commit 18c7c0f15149c445376d5457083ce2566db4f046.

--- src/GestaoRecursos/GestaoContext.cs
+++ src/GestaoRecursos/GestaoContext.cs
@@ -1,5 +1,4 @@
-﻿using GestaoRecursos.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Models;
 namespace Context;
@@ -15,23 +14,17 @@ public class GestaoContext : IdentityDbContext
     public DbSet<Venda> Vendas { get; set; }
     public DbSet<Compra> Compra { get; set; }
     public DbSet<Venda> Venda { get; set; }
-    public DbSet<Usuario> Usuarios { get; set; }
-    public DbSet<PerfilUsuario> PerfilUsuarios { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Produto>(entity => entity.HasMany(x => x.Vendas).WithOne(x => x.Produto));
         modelBuilder.Entity<Venda>(modelBuilder => modelBuilder.HasOne(x => x.Produto).WithMany(x => x.Vendas).HasForeignKey(x => x.ProdutoId));
-
+        
         modelBuilder.Entity<Produto>(entity => entity.HasMany(x => x.Compras).WithOne(x => x.Produto));
         modelBuilder.Entity<Compra>(modelBuilder => modelBuilder.HasOne(x => x.Produto).WithMany(x => x.Compras).HasForeignKey(x => x.ProdutoId));
 
         modelBuilder.Entity<Produto>(entity => entity.HasOne(x => x.TipoProduto).WithMany(x => x.Produtos).HasForeignKey(x => x.TipoProdutoId));
         modelBuilder.Entity<TipoProduto>(entity => entity.HasMany(x => x.Produtos).WithOne(x => x.TipoProduto));
-
-        modelBuilder.Entity<PerfilUsuario>().ToTable("Perfis");
-        modelBuilder.Entity<Usuario>().HasOne(u => u.PerfilUsuario).WithMany(p => p.Usuarios).HasForeignKey(u => u.PerfilUsuarioId);
-        modelBuilder.Entity<PerfilUsuario>().HasMany(p => p.Usuarios).WithOne(u => u.PerfilUsuario);
     }
 
 
